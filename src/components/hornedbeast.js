2e9bf0b3112062823bf34c0688a7e577ce4b715e
Coriana Williams <corianacory@gmail.com>
2023-04-25 19:47:19 -0400

    edits to horned beast

--- src/components/hornedbeast.js
+++ src/components/hornedbeast.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image';
+// import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 class HornedBeast extends React.Component {
   constructor(props) {
